086d0f4a2be9f7b599dd9333220b024dddef749d
jonathonlefler <lefler.t.jonathon@gmail.com>
2019-03-17 18:08:38 +0000

    cleaned up main.c and added LINESIZE global

--- examples/transmit/main.c
+++ examples/transmit/main.c
@@ -29,7 +29,6 @@
 
 #include "lmic.h"
 #include "debug.h"
-
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -40,14 +39,14 @@
 //////////////////////
 // GLOBAL VARIABLES //
 //////////////////////
-
-char customData[1000][11];
+#define LINESIZE 11 // LINESIZE = [hexa]digit count + 1
+char customData[1000][LINESIZE];
 int size;
 static const char* fn;
+
 //////////////////////////////////////////////////
 // CONFIGURATION (FOR APPLICATION CALLBACKS BELOW)
 //////////////////////////////////////////////////
-
 // application router ID (LSBF)
 static const u1_t APPEUI[8]  = { 0x16, 0x77, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
 
@@ -57,12 +56,9 @@ static const u1_t DEVEUI[8]  = { 0x4B, 0x7B, 0xAB, 0x3B, 0x66, 0xDF, 0x99, 0x00 
 // device-specific AES key (derived from device EUI)
 static const u1_t DEVKEY[16] = { 0x63, 0xD2, 0xCB, 0xC4, 0x31, 0x3D, 0xD8, 0x2F, 0x4A, 0xF8, 0xED, 0x99, 0x65, 0xA0, 0x49, 0xF7 };
 
-
-
 //////////////////////////////////////////////////
 // APPLICATION CALLBACKS
 //////////////////////////////////////////////////
-
 // provide application router ID (8 bytes, LSBF)
 void os_getArtEui (u1_t* buf) {
     memcpy(buf, APPEUI, 8);
@@ -78,26 +74,24 @@ void os_getDevKey (u1_t* buf) {
     memcpy(buf, DEVKEY, 16);
 }
 
-
 //////////////////////////////////////////////////
 // MAIN - INITIALIZATION AND STARTUP
 //////////////////////////////////////////////////
-
 // initial job
 static void initfunc (osjob_t* j) {
     // Load log.out into customData[]
-    // immediately prepare next transmissio
     fn = "/home/pi/MasterCode/transmit/log.out";
+    char buffer[LINESIZE];
+    int linecount = 0;
+    int rc;
     int fd = open(fn, O_RDWR);
+
     if(fd < 0) {
 	    perror(fn);
 	    exit(EXIT_FAILURE);
     }
-    char buffer[11];
-    int linecount = 0;
-    int rc;
     do {
-	    rc = read(fd, &customData[linecount], 11);
+	    rc = read(fd, &customData[linecount], LINESIZE);
 	    if (rc < 0) {
 		    perror("bad stuff.");
 	            exit(EXIT_FAILURE);
@@ -105,21 +99,19 @@ static void initfunc (osjob_t* j) {
 	    customData[linecount][rc-1] = 0;
 	    debug_str(customData[linecount]);
 	    linecount++;
-    } while (rc == 11);
-    // reset MAC stat
+    } while (rc == LINESIZE);
     close(fd);
     size = linecount - 2;
+    // reset MAC stat
     LMIC_reset();
     // start joining
     LMIC_startJoining();
     // init done - onEvent() callback will be invoked...
 }
 
-
 // application entry point
 int main () {
     osjob_t initjob;
-    
     // initialize runtime env
     os_init();
     // initialize debug library
@@ -131,16 +123,12 @@ int main () {
     // (not reached)
     return 0;
 }
-
-
 //////////////////////////////////////////////////
 // LMIC EVENT CALLBACK
 //////////////////////////////////////////////////
-
 void onEvent (ev_t ev) {
     debug_event(ev);
     if(size < 0){
-	    debug_str("/////////////FINISHED///////////");
 	    int removedStatus = remove(fn); //start/transmit.out delete file
 	    if(removedStatus != 0){
 	    	perror("Could not delete log.out");
@@ -150,23 +138,21 @@ void onEvent (ev_t ev) {
     
     switch(ev) {
         
-        // network joined, session established
-        case EV_JOINED:
+        case EV_JOINED: // network joined, session established
         debug_val("netid = ", LMIC.netid);
         goto tx;
         
-        // scheduled data sent (optionally data received)
-        case EV_TXCOMPLETE:
+        case EV_TXCOMPLETE: // scheduled data sent (optionally data received)
         if(LMIC.dataLen) { // data received in rx slot after tx
             debug_buf(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
         }
     tx:
-        for(int i = 0; i < 10; i++) { //start/loading LMIC.frame and sending
+        for(int i = 0; i < LINESIZE - 1; i++) { //start/loading LMIC.frame and sending
             LMIC.frame[i] = customData[size][i];
         }
-        LMIC_setTxData2(1, LMIC.frame, 10, 1); //end/loading LMIC.frame and sending
-        // (will be sent as soon as duty cycle permits)
-	size--;
+        LMIC_setTxData2(1, LMIC.frame, LINESIZE - 1, 1); //end/loading LMIC.frame and sending
+	size--; //customData[size] has been sent, now decrement size and move on to the next
         break;
     }
 }
+
